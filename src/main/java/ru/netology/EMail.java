c48f2c0de0266c410ad0697a214b8d61e5ab8b48
TMAdevby <mtipun@mail.ru>
2025-11-29 15:38:56 +0300

    email2

--- src/main/java/ru/netology/EMail.java
+++ src/main/java/ru/netology/EMail.java
@@ -1,6 +1,10 @@
 package ru.netology;
 
+import java.util.Scanner;
+
 public class EMail {
+
+    Scanner sc = new Scanner(System.in);
 
     private String firstName;
     private String lastName;
@@ -19,6 +23,34 @@ public class EMail {
         this.email = setEmail();
 
         this.password = createPassword();
+    }
+
+    public String setDepartment(){
+        System.out.println("Введите номер департамента.\n1.Департамент продаж\n2.Разработка\n" +
+                "3.Бухгалтерия\n0.Нет департамента");
+        String depNumber = sc.next();
+
+        boolean flag = false;
+        do{
+            switch (depNumber) {
+                case "1":
+                    department = "Департамент продаж";
+                    break;
+                case "2":
+                    department = "Разработка";
+                    break;
+                case "3":
+                    department = "Бухгалтерия";
+                    break;
+                case "0":
+                    department = "Нет департамента";
+                    break;
+                default:
+                    System.out.println("Некорректный ввод, повторите");
+                    flag = true;
+            }
+        }while (flag);
+        return department;
     }
 
 
