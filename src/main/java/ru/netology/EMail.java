0783d597784a36fe84c4a54571d5eca89b701563
TMAdevby <mtipun@mail.ru>
2025-11-29 14:47:58 +0300

    Initial

---
+++ src/main/java/ru/netology/EMail.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+public class EMail {
+
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String email;
+    private String password;
+    private int mailCapacity;
+    private String spare_email;
+}
---
+++ src/main/java/ru/netology/EmailMain.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class EmailMain {
+    public static void main(String[] args) {
+
+
+    }
+}
